c829b1f63e630a039b8ff55e39953171fd5522b2
Valdemar Rasmusson <valle.rasmusson@gmail.com>
2026-01-26 14:24:46 +0100

    Gjorde lite refaktiorisering på testerna

--- frontend/src/pages/RegistreringSida.test.tsx
+++ frontend/src/pages/RegistreringSida.test.tsx
@@ -3,15 +3,18 @@ import { describe, expect, it } from 'vitest';
 
 import RegistreringSida from './RegistreringSida';
 
-const input = screen.getByPlaceholderText('Skriv startnummer här');
+let input: HTMLElement;
+
+beforeEach(() => {	
+	render(<RegistreringSida />);
+	input = screen.getByPlaceholderText('Skriv startnummer här');
+});
+
 
 describe('RegisteringSida', () => {
 	it('Testar korrekt registrering', () => {
 
-		render(<RegistreringSida />);
-
 		// Skriv in siffror fältet
-		
 		fireEvent.change(input, { target : { value: '001'}})
 			
 		// Klicka på knappen
@@ -23,10 +26,8 @@ describe('RegisteringSida', () => {
 	});
 	
 	it('Testar felaktig registrering med bokstäver', () => {
-
-		render(<RegistreringSida />);
 	
-			// Skriv in bokstäver i fältet
+		// Skriv in bokstäver i fältet
 		fireEvent.change(input, { target : { value: 'abc'}})
 
 		// Klicka på knappen
@@ -38,32 +39,28 @@ describe('RegisteringSida', () => {
 	});
 
 	it('Testar registrering med extra nollor framför', () => {
-
-		render(<RegistreringSida />);
 		
-			// Skriv in med nollor framför
+		// Skriv in med nollor framför
 		fireEvent.change(input, { target : { value: '000210'}})
 
 		// Klicka på knappen
 		fireEvent.click(screen.getByText('Registrera'));
 
 		// Efter klick kolla listan
-		expect(screen.getByText('Startnummer: 210', {exact:false})).not.toBeInTheDocument();
+		expect(screen.getByText('Startnummer: 210', {exact:false})).toBeInTheDocument();
 	
 	});
 
 	it('Testar registrering med färre än tre siffror', () => {
 
-		render(<RegistreringSida />);
-		
-			// Skriv in utan extra nollor
+		// Skriv in utan extra nollor
 		fireEvent.change(input, { target : { value: '13'}})
 
 		// Klicka på knappen
 		fireEvent.click(screen.getByText('Registrera'));
 
 		// Efter klick kolla listan
-		expect(screen.getByText('Startnummer: 013', {exact:false})).not.toBeInTheDocument();
+		expect(screen.getByText('Startnummer: 013', {exact:false})).toBeInTheDocument();
 	
 	});
 });
