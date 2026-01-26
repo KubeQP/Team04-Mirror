0bd1f06a39ffd0e69ce1f76c512b785ea7881fc4
ellyhedinoleniusson <144734388+ellyhedinoleniusson@users.noreply.github.com>
2026-01-26 10:35:19 +0100

    Working on global settings table css

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -1,5 +1,5 @@
 // frontend/src/pages/Admin.tsx
-import { useState } from 'react';
+//import { useState } from 'react';
 
 // src/pages/Admin.tsx
 export default function Admin(){
@@ -7,74 +7,76 @@ export default function Admin(){
     return(
         <div>
             <h2>Admin Sida</h2>
-            <p>Välkommen till administrationssidan.</p>
+            <p>Välkommen till administrationssidan.</p>   
 
-            {/* Första table */}
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nr.</th>
-                        <th>Namn</th>
-                        <th>Start</th>
-                        <th>Mål</th>
-                        <th>Tid</th>
-                        <th>Totalt</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>AA</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>BB</td>
-                        <td>-</td>
-                        <td>2 tider!</td>
-                        <td>-</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>CC</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>-</td>
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td>DD</td>
-                        <td>-</td>
-                        <td>X</td>
-                        <td>?</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className = "a">
+                {/* Första table */}
+                <table className = "test">
+                    <thead>
+                        <tr>
+                            <th>Nr.</th>
+                            <th>Namn</th>
+                            <th>Start</th>
+                            <th>Mål</th>
+                            <th>Tid</th>
+                            <th>Totalt</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>AA</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>BB</td>
+                            <td>-</td>
+                            <td>2 tider!</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>CC</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>DD</td>
+                            <td>-</td>
+                            <td>X</td>
+                            <td>?</td>
+                        </tr>
+                    </tbody>
+                </table>
 
-            {/* Station table */}
-            <table>
-                <thead>
-                    <tr>
-                        <th>Station</th>
-                        <th>Nr.</th>
-                        <th>Tid</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>S</td>
-                        <td>1</td>
-                        <td>-</td>
-                    </tr>
-                    <tr>
-                        <td>S</td>
-                        <td>2</td>
-                        <td>-</td>
-                    </tr>
-                </tbody>
-            </table>
+                {/* Station table */}
+                <table className = "b">
+                    <thead>
+                        <tr>
+                            <th>Station</th>
+                            <th>Nr.</th>
+                            <th>Tid</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>S</td>
+                            <td>1</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>S</td>
+                            <td>2</td>
+                            <td>-</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
