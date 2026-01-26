c9987e4d825cd05b362bf4781c4a889d08283636
Valdemar Rasmusson <valle.rasmusson@gmail.com>
2026-01-26 10:37:21 +0100

    Implemented a working regList

--- frontend/src/pages/RegistreringSida.tsx
+++ frontend/src/pages/RegistreringSida.tsx
@@ -5,9 +5,13 @@ export default function RegisteringSida(){
 
 
   const [reg, setReg] = useState("");
-  const regLista: string[] = [];
+  //const regLista: string[] = [];
+  const [regLista, setRegLista] = useState<string[]>([]);
   const addReg = () => {
-    regLista.push(reg);
+    if (!reg.trim()) return;
+
+    setRegLista((prev) => [...prev, reg]);
+    setReg('');
   };
 
   return (
@@ -15,8 +19,11 @@ export default function RegisteringSida(){
 			<h2>Registrering:</h2>
 			<input id="startNbrInput" value={reg} onChange= { (e) => setReg(e.target.value)} type="text" placeholder="Skriv startnummer här" />
       <button onClick={addReg}>Registrera</button>
-      <ul id="registrationsList"></ul>
-      
+      <ul>
+        {regLista.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
 		</div>
   );
 
