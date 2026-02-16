be8aa255d1ceb619418a533ed5515aa945c5c45d
Simon Mechler <simon.mechler.4031@student.lu.se>
2026-02-16 10:01:57 +0100

    update admin page to allow editing

--- frontend/src/pages/RegistreringStoppTid.tsx
+++ frontend/src/pages/RegistreringStoppTid.tsx
@@ -82,43 +82,6 @@ export default function RegistreringStoppTid() {
 		const res = await getStationData();
 		setStations(res);
 	};
-	/*
-  const fetchStations = async () => {
-    const res = await fetch(`${API_BASE_URL}/stations/getstations`);
-    if (!res.ok) return;
-    const data: Station[] = await res.json();
-    setStations(data);
-
-    if (selectedStationId === "" && data.length > 0) {
-      setSelectedStationId(data[0].id);
-    }
-  };
-
-	const fetchCompetitors = async () => {
-		try {
-			const res = await fetch(`${API_BASE_URL}/competitors/`);
-			if (!res.ok) {
-				setMsg(`Kunde inte hämta tävlande (status ${res.status})`);
-				return;
-			}
-			const data: Competitor[] = await res.json();
-			setCompetitors(data);
-
-			// Om inget valt än, välj första i listan
-			if (!selectedStartNumber && data.length > 0) {
-				setSelectedStartNumber(data[0].start_number);
-			}
-
-      // Om det valda startnumret inte längre finns (t.ex. reset av DB), välj första
-      if (selectedStartNumber && !data.some(c => c.start_number === selectedStartNumber)) {
-        setSelectedStartNumber(data[0]?.start_number ?? "");
-      }
-    } catch (err) {
-      console.error(err);
-      setMsg("Kunde inte kontakta servern för att hämta tävlande.");
-    }
-  };
-  */
 
 	// Hämta vid första mount
 	useEffect(() => {
