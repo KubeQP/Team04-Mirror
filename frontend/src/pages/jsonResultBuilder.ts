ab68b45161b0a3761fdbc9fc88ce8a870704c14e
Simon Mechler <simon.mechler.4031@student.lu.se>
2026-02-23 08:48:34 +0100

    run formatter and linter

--- frontend/src/pages/jsonResultBuilder.ts
+++ frontend/src/pages/jsonResultBuilder.ts
@@ -1,8 +1,8 @@
-export interface Result{
-    place: string;
-    startNbr: string;
-    name: string;
-    totalTime: string;
-    startTime: string;
-    endTime: string;
+export interface Result {
+	place: string;
+	startNbr: string;
+	name: string;
+	totalTime: string;
+	startTime: string;
+	endTime: string;
 }
