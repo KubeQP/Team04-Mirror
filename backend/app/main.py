b0e6532116fa1a7e101717b87e787554476a989c
LukasSmeds <lukassmeds@gmail.com>
2026-02-23 14:14:58 +0100

    Fixed back from 5177 to 5173

--- backend/app/main.py
+++ backend/app/main.py
@@ -146,8 +146,8 @@ app = FastAPI(lifespan=lifespan, title="Race Timing Backend API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5177",
-        "http://localhost:5177",
+        "http://127.0.0.1:5173",
+        "http://localhost:5173",
         "http://127.0.0.1:8000",
         "http://localhost:8000",
     ],
