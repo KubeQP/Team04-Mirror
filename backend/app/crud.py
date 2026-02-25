1e5e82a603d46accd42d141eb6765591164d620a
Valdemar Rasmusson <valle.rasmusson@gmail.com>
2026-02-26 00:12:54 +0100

    samma igen...

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import Session
 
 from .models import Competitor, Station, TimeEntry
 
-from typing import Any
+from typing import Any, cast
 
 
 def get_competitors(db: Session) -> list[Competitor]:
@@ -81,7 +81,7 @@ def update_station_order(db: Session, stations: list[dict[str, Any]]) -> None:
         )
 
         if station:
-            station.order = int(station_data["order"])
+            station.order = cast(int(station_data["order"]))
 
     db.commit()
 
