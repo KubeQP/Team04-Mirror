e26fbf42ca632739a0913092db6f704f1f716bd2
Gustaf <gustaf@wahlgren.info>
2026-03-02 08:32:33 +0100

    change ip to live server

--- env.config.js
+++ env.config.js
@@ -1,4 +1,4 @@
 // Use this command to update both frontend and backend: node sync-env.js
 module.exports = {
-  "API_BASE_URL": "http://100.84.14.107:8000",
+  "API_BASE_URL": "https://pvg-race.cs.lth.se/:8004",
 }
